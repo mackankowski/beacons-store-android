d5baaf3db36861a98c7826670b399288d0cd37f8
Maciej Kankowski <mackankowski@gmail.com>
2018-12-02 20:55:25 +0100

    Refactor

--- app/src/main/java/pl/mackan/beaconstore/Beacon.kt
+++ app/src/main/java/pl/mackan/beaconstore/Beacon.kt
@@ -1,14 +1,8 @@
 package pl.mackan.beaconstore
 
-import android.app.Activity
 import android.content.Context
 import android.util.Log
-import com.estimote.proximity_sdk.api.EstimoteCloudCredentials
-import com.estimote.proximity_sdk.api.ProximityObserverBuilder
-import com.estimote.proximity_sdk.api.ProximityObserver
-import com.estimote.proximity_sdk.api.ProximityZoneBuilder
-import com.estimote.proximity_sdk.api.ProximityZone
-import com.estimote.mustard.rx_goodness.rx_requirements_wizard.RequirementsWizardFactory
+import com.estimote.proximity_sdk.api.*
 
 class Beacon {
 
@@ -17,15 +11,16 @@ class Beacon {
         private var INSTANCE: Beacon? = null
         val cloudCredentials = EstimoteCloudCredentials(Estimote.appId, Estimote.appToken)
         var proximityObserver: ProximityObserver? = null
-        val logTag = "beacon-store"
+        var zone :ProximityZone? = null
+        val logTag = "beacon-store-logger"
 
-        fun getInstance(context: Context, activity: Activity) {
+        fun getInstance(context: Context) {
             INSTANCE ?: synchronized(this) {
-                INSTANCE ?: buildProximityObserver(context, activity).also { INSTANCE }
+                INSTANCE ?: buildProximityObserver(context).also { INSTANCE }
             }
         }
 
-        fun buildProximityObserver(context: Context, activity: Activity) {
+        fun buildProximityObserver(context: Context) {
             Log.i(logTag, "buildProximityObserver()");
             proximityObserver = ProximityObserverBuilder(context, cloudCredentials)
                     .onError { throwable ->
@@ -35,7 +30,7 @@ class Beacon {
                     .withBalancedPowerMode()
                     .build()
 
-            val zone = ProximityZoneBuilder()
+            zone = ProximityZoneBuilder()
                     .forTag("desks")
                     .inNearRange()
                     .onEnter { context ->
@@ -48,31 +43,6 @@ class Beacon {
                         null
                     }
                     .build()
-            checkPermission(activity, zone)
         }
-
-        fun checkPermission(activity: Activity, zone: ProximityZone) {
-            Log.i(logTag, "checkPermission()");
-            RequirementsWizardFactory
-                    .createEstimoteRequirementsWizard()
-                    .fulfillRequirements(activity,
-                            // onRequirementsFulfilled
-                            {
-                                Log.d(logTag, "requirements fulfilled")
-                                proximityObserver?.startObserving(zone)
-                                null
-                            },
-                            // onRequirementsMissing
-                            { requirements ->
-                                Log.e(logTag, "requirements missing: $requirements")
-                                null
-                            }
-                            // onError
-                    ) { throwable ->
-                        Log.e(logTag, "requirements error: $throwable")
-                        null
-                    }
-        }
-
     }
 }
--- app/src/main/java/pl/mackan/beaconstore/activities/MainActivity.kt
+++ app/src/main/java/pl/mackan/beaconstore/activities/MainActivity.kt
@@ -3,32 +3,59 @@ package pl.mackan.beaconstore.activities
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import pl.mackan.beaconstore.R
 import com.google.android.gms.auth.api.credentials.Credential
 import com.google.android.gms.auth.api.credentials.CredentialRequest
 import com.google.android.gms.auth.api.credentials.Credentials
 import com.google.android.gms.auth.api.credentials.CredentialsClient
 import android.widget.Toast
+import com.estimote.mustard.rx_goodness.rx_requirements_wizard.RequirementsWizardFactory
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.FirebaseAuth
 import pl.mackan.beaconstore.Beacon
+import pl.mackan.beaconstore.Beacon.Companion.proximityObserver
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
-    private lateinit var beacon: Unit
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         auth = FirebaseAuth.getInstance()
-        beacon = Beacon.getInstance(applicationContext, this)
+        checkLocationPermissions()
     }
 
     override fun onResume() {
         super.onResume()
         getCredentials()
+    }
+
+    private fun checkLocationPermissions(){
+        Log.i(Beacon.logTag, "checkPermission()");
+        RequirementsWizardFactory
+                .createEstimoteRequirementsWizard()
+                .fulfillRequirements(this,
+                        // onRequirementsFulfilled
+                        {
+                            Log.d(Beacon.logTag, "requirements fulfilled")
+                            proximityObserver?.startObserving(Beacon.zone!!)
+                            null
+                        },
+                        // onRequirementsMissing
+                        { requirements ->
+                            Log.e(Beacon.logTag, "requirements missing: $requirements")
+                            null
+                        }
+                        // onError
+                ) { throwable ->
+                    Log.e(Beacon.logTag, "requirements error: $throwable")
+                    null
+                }
+
     }
 
     private fun getCredentials() {
